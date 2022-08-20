ead7b2b2fd2a493a82e0e87457a5f45f36d6c2d6
chrischep <christine.chepleting@student.moringaschool.com>
2022-08-20 12:53:43 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 // Write your code here
+let num1=2;
+let num2=31;
+let multiply =num1*num2;
+function multiplication(){
+
+
+if(num1*num2===62){
+    return'correct'
+}else{
+    return'not valid'
+}
+}
+let random=10
+Math.floor(Math.random() * 10) + 1;
+
+let num3=22;
+let num4=18;
+let mod=4;
+function modulus(){
+    if(num3%num4===4){
+        return'correct'
+    }
+    else{
+        return'wrong'
+    }
+}
+let max=20;
+Math.max(2, 6, 18, 10, 15);
